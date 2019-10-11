c2bc1bfc745b1ebeef3d4a63b045fa2de1d797c0
Jacqueline Shay <Jacqueline.A.Shay@gmail.com>
2019-10-11 17:16:37 -0500

    tired

--- regression/explore.py
+++ regression/explore.py
@@ -18,13 +18,18 @@ import env
 # 	g.map_offdiag(plt.scatter)
 # 	return g
 
+g = sns.PairGrid(train)# create grids
+g.map_diag(plt.hist) # fill in histogram in diagnal
+g.map_offdiag(plt.scatter) # fill in remaining with scatter
+
+
+
 def months_to_years():
 	df = wrangle.wrangle_telco().set_index("customer_id")
 	df["tenure_years"] = round(df.tenure / 12).astype('category')
 	return df
 
-def months_to_years()
-sns.violinplot(x="tenure_years", y="total_charges", data=df)
-# violin plot
-# bar
-# box
+def plot_categorical_and_continous_vars():
+	v = sns.violinplot(x="tenure_years", y="total_charges", data=df)
+	b = sns.boxplot(x="tenure_years", y="total_charges", data=df)
+	b = sns.barplot(x="tenure_years", y="total_charges", data=df)
--- regression/wrangle.py
+++ regression/wrangle.py
@@ -33,9 +33,6 @@ import env
 
 
 
-
-
-
 def get_db_url(db):
     return f'mysql+pymysql://{env.user}:{env.password}@{env.host}/{db}'
 
@@ -50,39 +47,3 @@ def wrangle_telco():
     reg.total_charges = reg.total_charges.str.strip().replace('', np.nan).astype(float)
     reg = reg.dropna()
     return reg
-
-
-
-
-
-# from env import host, user, password
-
-# def get_db_url(db):
-#     url = f'mysql+pymysql://{user}:{password}@{host}/{database_name}'
-#     return url
-
-# def get_data_from_mysql():
-# 	query = '''
-# 	SELECT *
-# 	FROM customers
-# 	JOIN internet_service_types USING (internet_service_type_id)
-# 	WHERE internet_service_type_id = 3
-# 	'''
-# 	df = pd.read_sql(query,get_db_url('telco_churn'))
-# 	return df
-
-# def clean_data(df):
-# 	df = df[["customer_id", "tenure", "monthly_charges", "total_charges"]]
-# 	df.total_charges = df.total_charges.str.strip().replace('',np.nan).astype(float)
-# 	df = df.dropna()
-# 	return df
-
-# def wrangle_telco():
-# 	df = get_data_from_mysql()
-# 	df = clean_data(df)
-# 	return df
-# 	# alternative
-# 	# return clean_data(get_data_from_mysql())
-
-# # import wrangle
-# # wrangle.wrangle_telco()
