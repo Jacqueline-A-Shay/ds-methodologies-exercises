4bbf672f7d494580e0b41d84cdd75dd1fda745c6
Jacqueline Shay <Jacqueline.A.Shay@gmail.com>
2019-10-11 10:57:51 -0500

    need to touch explore and split_scale

---
+++ regression/explore.py
@@ -0,0 +1,30 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+import wrangle
+import env
+
+
+# def plot_variable_pairs():
+# 	df = wrangle.wrangle_telco().set_index("customer_id")
+# 	g = sns.pairplot(df, kind = 'reg')
+# 	g.map_diag(plt.hist)
+# 	g.map_offdiag(plt.scatter)
+# 	return g
+
+def months_to_years():
+	df = wrangle.wrangle_telco().set_index("customer_id")
+	df["tenure_years"] = round(df.tenure / 12).astype('category')
+	return df
+
+def months_to_years()
+sns.violinplot(x="tenure_years", y="total_charges", data=df)
+# violin plot
+# bar
+# box
--- regression/split_scale.py
+++ regression/split_scale.py
@@ -26,54 +26,55 @@ def perform_standard_scaler(X_train, X_test, y_train, y_test):
 	X_test_scaled = pd.DataFrame(X_std_scaler.transform(X_test), columns=X_test.columns.values).set_index([X_test.index.values])
 	y_train_scaled = pd.DataFrame(y_std_scaler.transform(y_train), columns=y_train.columns.values).set_index([y_train.index.values])
 	y_test_scaled = pd.DataFrame(y_std_scaler.transform(y_test), columns=y_test.columns.values).set_index([y_test.index.values])
-	return (X_std_scaler, y_std_scaler, X_train_scaled, X_test_scaled, y_train_scaled, y_test_scaled)
+	return X_train_scaled
+	#return (X_std_scaler, y_std_scaler, X_train_scaled, X_test_scaled, y_train_scaled, y_test_scaled)
 def split_scale():
 	a, b, c, d = obtain_data()
 	return perform_standard_scaler(a, b, c, d)
 
-# uniform
-def perform_uniform_scaler(X_train, X_test, y_train, y_test):
-    X_u_scaler = PowerTransformer(method='yeo-johnson', standardize=False, copy=True).fit(X_train)
-    y_u_scaler = PowerTransformer(method='yeo-johnson', standardize=False, copy=True).fit(y_train)
+# # uniform
+# def perform_uniform_scaler(X_train, X_test, y_train, y_test):
+#     X_u_scaler = PowerTransformer(method='yeo-johnson', standardize=False, copy=True).fit(X_train)
+#     y_u_scaler = PowerTransformer(method='yeo-johnson', standardize=False, copy=True).fit(y_train)
 
-    X_train_scaled = pd.DataFrame(x_std_scaler.transform(X_train), columns=X_train.columns.values).set_index([X_train.index.values])
-    X_test_scaled = pd.DataFrame(x_std_scaler.transform(X_test), columns=X_test.columns.values).set_index([X_test.index.values])
+#     X_train_scaled = pd.DataFrame(x_std_scaler.transform(X_train), columns=X_train.columns.values).set_index([X_train.index.values])
+#     X_test_scaled = pd.DataFrame(x_std_scaler.transform(X_test), columns=X_test.columns.values).set_index([X_test.index.values])
 
-    y_train_scaled = pd.DataFrame(y_std_scaler.transform(y_train), columns=y_train.columns.values).set_index([y_train.index.values])
-    y_test_scaled = pd.DataFrame(y_std_scaler.transform(y_test), columns=y_test.columns.values).set_index([y_test.index.values])
+#     y_train_scaled = pd.DataFrame(y_std_scaler.transform(y_train), columns=y_train.columns.values).set_index([y_train.index.values])
+#     y_test_scaled = pd.DataFrame(y_std_scaler.transform(y_test), columns=y_test.columns.values).set_index([y_test.index.values])
 
-# power
-# create scaler object using yeo-johnson method and fit to train
-X_p_scaler = PowerTransformer(method='yeo-johnson', standardize=False, copy=True).fit(X_train)
-y_p_scaler = PowerTransformer(method='yeo-johnson', standardize=False, copy=True).fit(y_train)
+# # power
+# # create scaler object using yeo-johnson method and fit to train
+# X_p_scaler = PowerTransformer(method='yeo-johnson', standardize=False, copy=True).fit(X_train)
+# y_p_scaler = PowerTransformer(method='yeo-johnson', standardize=False, copy=True).fit(y_train)
 
-X_train_p_scaled = pd.DataFrame(X_p_scaler.transform(X_train), columns=X_train.columns.values).set_index([X_train.index.values])
-X_test_p_scaled = pd.DataFrame(X_p_scaler.transform(X_test), columns=X_test.columns.values).set_index([X_test.index.values])
+# X_train_p_scaled = pd.DataFrame(X_p_scaler.transform(X_train), columns=X_train.columns.values).set_index([X_train.index.values])
+# X_test_p_scaled = pd.DataFrame(X_p_scaler.transform(X_test), columns=X_test.columns.values).set_index([X_test.index.values])
 
-y_train_p_scaled = pd.DataFrame(y_p_scaler.transform(y_train), columns=y_train.columns.values).set_index([y_train.index.values])
-y_test_p_scaled = pd.DataFrame(y_p_scaler.transform(y_test), columns=y_test.columns.values).set_index([y_test.index.values])
+# y_train_p_scaled = pd.DataFrame(y_p_scaler.transform(y_train), columns=y_train.columns.values).set_index([y_train.index.values])
+# y_test_p_scaled = pd.DataFrame(y_p_scaler.transform(y_test), columns=y_test.columns.values).set_index([y_test.index.values])
 
-X_train_p_scaled.head()
+# X_train_p_scaled.head()
 
-# min_max
-X_mm_scaler = MinMaxScaler(copy=True, feature_range=(0,1)).fit(X_train)
-y_mm_scaler = MinMaxScaler(copy=True, feature_range=(0,1)).fit(y_train)
+# # min_max
+# X_mm_scaler = MinMaxScaler(copy=True, feature_range=(0,1)).fit(X_train)
+# y_mm_scaler = MinMaxScaler(copy=True, feature_range=(0,1)).fit(y_train)
 
-X_train_mm_scaled = pd.DataFrame(X_mm_scaler.transform(X_train), columns=X_train.columns.values).set_index([X_train.index.values])
-X_test_mm_scaled = pd.DataFrame(X_mm_scaler.transform(X_test), columns=X_test.columns.values).set_index([X_test.index.values])
+# X_train_mm_scaled = pd.DataFrame(X_mm_scaler.transform(X_train), columns=X_train.columns.values).set_index([X_train.index.values])
+# X_test_mm_scaled = pd.DataFrame(X_mm_scaler.transform(X_test), columns=X_test.columns.values).set_index([X_test.index.values])
 
-y_train_mm_scaled = pd.DataFrame(y_mm_scaler.transform(y_train), columns=y_train.columns.values).set_index([y_train.index.values])
-y_test_mm_scaled = pd.DataFrame(y_mm_scaler.transform(y_test), columns=y_test.columns.values).set_index([y_test.index.values])
+# y_train_mm_scaled = pd.DataFrame(y_mm_scaler.transform(y_train), columns=y_train.columns.values).set_index([y_train.index.values])
+# y_test_mm_scaled = pd.DataFrame(y_mm_scaler.transform(y_test), columns=y_test.columns.values).set_index([y_test.index.values])
 
-X_train_mm_scaled.head()
+# X_train_mm_scaled.head()
 
-# robust
-X_r_scaler = RobustScaler(quantile_range=(25.0,75.0), copy=True, with_centering=True, with_scaling=True).fit(X_train)
-y_r_scaler = RobustScaler(quantile_range=(25.0,75.0), copy=True, with_centering=True, with_scaling=True).fit(y_train)
+# # robust
+# X_r_scaler = RobustScaler(quantile_range=(25.0,75.0), copy=True, with_centering=True, with_scaling=True).fit(X_train)
+# y_r_scaler = RobustScaler(quantile_range=(25.0,75.0), copy=True, with_centering=True, with_scaling=True).fit(y_train)
 
-X_train_r_scaled = pd.DataFrame(X_r_scaler.transform(X_train), columns=X_train.columns.values).set_index([X_train.index.values])
-X_test_r_scaled = pd.DataFrame(X_r_scaler.transform(X_test), columns=X_test.columns.values).set_index([X_test.index.values])
+# X_train_r_scaled = pd.DataFrame(X_r_scaler.transform(X_train), columns=X_train.columns.values).set_index([X_train.index.values])
+# X_test_r_scaled = pd.DataFrame(X_r_scaler.transform(X_test), columns=X_test.columns.values).set_index([X_test.index.values])
 
-y_train_r_scaled = pd.DataFrame(y_r_scaler.transform(y_train), columns=y_train.columns.values).set_index([y_train.index.values])
-y_test_r_scaled = pd.DataFrame(y_r_scaler.transform(y_test), columns=y_test.columns.values).set_index([y_test.index.values])
+# y_train_r_scaled = pd.DataFrame(y_r_scaler.transform(y_train), columns=y_train.columns.values).set_index([y_train.index.values])
+# y_test_r_scaled = pd.DataFrame(y_r_scaler.transform(y_test), columns=y_test.columns.values).set_index([y_test.index.values])
 
