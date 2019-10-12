600fd265359a77f136a1561eab2a3cdb01fb1066
Jacqueline Shay <Jacqueline.A.Shay@gmail.com>
2019-10-12 15:12:17 -0500

    finish explore

--- regression/explore.py
+++ regression/explore.py
@@ -5,18 +5,17 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 
 import wrangle
 import env
 
-
 def plot_variable_pairs(df):
-# 	df = wrangle.wrangle_telco().set_index("customer_id")
- 	g = sns.pairplot(df, kind = 'reg')
- 	g.map_diag(plt.hist)
- 	g.map_offdiag(plt.scatter)
- 	return g
+	g = sns.pairplot(data = df, kind = 'reg')
+	g.map_diag(plt.hist)
+	g.map_offdiag(plt.scatter, color='g', alpha = 0.01)
+	return g
 
 def months_to_years(df):
 	df["tenure_years"] = round(df.tenure / 12).astype('category')
@@ -24,10 +23,24 @@ def months_to_years(df):
 	return tenure_in_year
 
 def plot_categorical_and_continous_vars(tenure_in_year):
-	f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(20, 5), sharex=True)
+	fig = plt.figure(figsize=(25, 25))
+	gs = gridspec.GridSpec(4, 3)
+	sns.set(font_scale=1.4)
+	ax1 = plt.subplot(gs[0, 0])
+	ax2 = plt.subplot(gs[0, 1])
+	ax3 = plt.subplot(gs[0, -1])
+	ax4 = plt.subplot(gs[1: , :])
+
 	sns.violinplot(x="tenure_years", y="total_charges", data=tenure_in_year, ax = ax1)
 	sns.boxplot(x="tenure_years", y="total_charges", data=tenure_in_year, ax = ax2)
 	sns.barplot(x="tenure_years", y="total_charges", data=tenure_in_year, ax = ax3)
+	sns.heatmap(tenure_in_year.corr(),cmap='Blues',annot=True, ax = ax4)
 
-def final_plot(df):
-	return plot_categorical_and_continous_vars(months_to_years(df))
+def final_plot():
+	
+	df = wrangle.wrangle_telco().set_index("customer_id")
+	plot_pairs = plot_variable_pairs(df)
+	tenure_in_year = months_to_years(df)
+	plot_category = plot_categorical_and_continous_vars(tenure_in_year)
+
+	return plot_pairs, plot_category
