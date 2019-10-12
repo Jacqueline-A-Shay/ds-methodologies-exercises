0c5803d7fd327a4f132f53c423ea5128ce710290
Jacqueline Shay <Jacqueline.A.Shay@gmail.com>
2019-10-12 13:56:09 -0500

    final

--- regression/explore.py
+++ regression/explore.py
@@ -11,25 +11,23 @@ import wrangle
 import env
 
 
-# def plot_variable_pairs():
+def plot_variable_pairs(df):
 # 	df = wrangle.wrangle_telco().set_index("customer_id")
-# 	g = sns.pairplot(df, kind = 'reg')
-# 	g.map_diag(plt.hist)
-# 	g.map_offdiag(plt.scatter)
-# 	return g
+ 	g = sns.pairplot(df, kind = 'reg')
+ 	g.map_diag(plt.hist)
+ 	g.map_offdiag(plt.scatter)
+ 	return g
 
-g = sns.PairGrid(train)# create grids
-g.map_diag(plt.hist) # fill in histogram in diagnal
-g.map_offdiag(plt.scatter) # fill in remaining with scatter
-
-
-
-def months_to_years():
-	df = wrangle.wrangle_telco().set_index("customer_id")
+def months_to_years(df):
 	df["tenure_years"] = round(df.tenure / 12).astype('category')
-	return df
+	tenure_in_year = df
+	return tenure_in_year
 
-def plot_categorical_and_continous_vars():
-	v = sns.violinplot(x="tenure_years", y="total_charges", data=df)
-	b = sns.boxplot(x="tenure_years", y="total_charges", data=df)
-	b = sns.barplot(x="tenure_years", y="total_charges", data=df)
+def plot_categorical_and_continous_vars(tenure_in_year):
+	f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(20, 5), sharex=True)
+	sns.violinplot(x="tenure_years", y="total_charges", data=tenure_in_year, ax = ax1)
+	sns.boxplot(x="tenure_years", y="total_charges", data=tenure_in_year, ax = ax2)
+	sns.barplot(x="tenure_years", y="total_charges", data=tenure_in_year, ax = ax3)
+
+def final_plot(df):
+	return plot_categorical_and_continous_vars(months_to_years(df))
