9e20bf354435fa086e14fc8decf525851e1a669c
Jacqueline Shay <Jacqueline.A.Shay@gmail.com>
2019-10-13 11:41:35 -0500

    added baseline calc

--- regression/evaluate.py
+++ regression/evaluate.py
@@ -2,6 +2,7 @@ plot_residuals(x, y, dataframe)
 
 regression_errors(y, yhat)
 
+
 def regression_errors(x, y, df):
     from statsmodels.formula.api import ols
     # fit linear model
@@ -16,7 +17,36 @@ def regression_errors(x, y, df):
     MSE = SSE/ len(df)
     from math import sqrt
     RMSE = sqrt(MSE)
-    print("SSE: ", SSE, "MSE: ", MSE, "RMSE: ", RMSE)
     
-    return SSE, MSE, RMSE #ESS, TSS
+    # compare w/ baseline, if not better, no need model, just rely on mean or median
+
+    # copy dataframe
+    df_baseline = pd.DataFrame(x)
+    df_baseline['target'] = y
+    # compute the overall mean of the y values and add to 'yhat' as our prediction
+    df_baseline['yhat'] = df_baseline.target.mean()
+    
+    # SSE_baseline
+    # compute the difference between y and yhat
+    df_baseline['residual'] = df_baseline['yhat'] - df_baseline.target
+    # calc SSE_baseline from residual
+    SSE_baseline = sum(df_baseline.residual ** 2)
+    # MSE_baseline
+    MSE_baseline = SSE_baseline/len(df_baseline)
+    # RMSE_baseline
+    RMSE_baseline = sqrt(MSE_baseline)
+
+    # create dataframe holding summary
+    df_eval = pd.DataFrame(np.array(['SSE','MSE','RMSE']), columns=['metric'])
+    df_eval['model_error'] = np.array([SSE, MSE, RMSE])
+
+    df_eval['baseline_error'] = np.array([SSE_baseline, MSE_baseline, RMSE_baseline])
+    df_eval['error_delta'] = df_eval.model_error - df_eval.baseline_error
+   
+    print("SSE: ", SSE, "MSE: ", MSE, "RMSE: ", RMSE)
+    print("SSE_base: ", SSE_baseline, "MSE_base: ", MSE_baseline, "RMSE_base: ", RMSE_baseline)
+    
+    return df_eval, SSE, MSE, RMSE #ESS, TSS
+
+
 regression_errors(tip.total_bill, tip.tip, tip)
