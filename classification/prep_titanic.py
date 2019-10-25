80019aecd700915a2e9a3a2e96613590df43e1d8
Jacqueline Shay <Jacqueline.A.Shay@gmail.com>
2019-10-25 14:30:54 -0500

    touch

--- classification/prep_titanic.py
+++ classification/prep_titanic.py
@@ -1,48 +1,77 @@
 import pandas as pd
 import numpy as np
+import scipy as sp 
+
 import acquire
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, MinMaxScaler
 
 import acquire
-import split_scale
 
-# ignore warnings
 import warnings
 warnings.filterwarnings("ignore")
 
 def process_data(df):
-	
-	df.drop(columns = 'deck', inplace = True)
-	df.fillna(np.nan, inplace = True)
+	df.drop(columns=['passenger_id','embarked','deck'], inplace=True)
+	# need to fillna for imputer
+	df.fillna(np.nan, inplace=True)
 
 	from sklearn.model_selection import train_test_split
 	train, test = train_test_split(df, train_size = 0.8, random_state = 123)
+	return df, train, test
 
-	imp_mode = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
-	
-	imp_mode.fit(train[['embarked']])
-	train['embarked'] = imp_mode.transform(train[['embarked']])
-	test['embarked'] = imp_mode.transform(test[['embarked']])
+def impute(train, test, my_strategy, column_list):
+	imputer = SimpleImputer(strategy=my_strategy)
+	train[column_list] = imputer.fit_transform(train[column_list])
+	test[column_list] = imputer.transform(test[column_list])
+	return train, test
 
-	imp_mode.fit(train[['embark_town']])
-	train['embark_town'] = imp_mode.transform(train[['embark_town']])
-	test['embark_town'] = imp_mode.transform(test[['embark_town']])
+def encode(train, test, col_name):
+    
+    encoded_values = sorted(list(train[col_name].unique()))
+    
+    # Integer Encoding
+    int_encoder = LabelEncoder()
+    train.encoded = int_encoder.fit_transform(train[col_name])
+    test.encoded = int_encoder.transform(test[col_name])
+    
+    # create 2D np arrays of the encoded variable (in train and test)
+    train_array = np.array(train.encoded).reshape(len(train.encoded),1)
+    test_array = np.array(test.encoded).reshape(len(test.encoded),1)
 
-	imp_mode.fit(train[['age']])
-	train['age'] = imp_mode.transform(train[['age']])
-	test['age'] = imp_mode.transform(test[['age']])
+    # One Hot Encoding
+    ohe = OneHotEncoder(sparse=False, categories='auto')
+    train_ohe = ohe.fit_transform(train_array)
+    test_ohe = ohe.transform(test_array)
+    
+    # Turn the array of new values into a data frame with columns names being the values
+    # and index matching that of train/test
+    # then merge the new dataframe with the existing train/test dataframe
+    train_encoded = pd.DataFrame(data=train_ohe,
+                            columns=encoded_values, index=train.index)
+    train = train.join(train_encoded)
+    
+    test_encoded = pd.DataFrame(data=test_ohe,
+                               columns=encoded_values, index=test.index)
+    test = test.join(test_encoded)
+    
+    return train, test
 
 
-	LE = LabelEncoder()
-	LE.fit(train.embarked)
-	train['embarked'] = LE.transform(train[['embarked']])
-	test['embarked'] = LE.transform(test[['embarked']])
+def scale_minmax(train, test, column_list):
+    scaler = MinMaxScaler()
+    column_list_scaled = [col + '_scaled' for col in column_list]
+    train_scaled = pd.DataFrame(scaler.fit_transform(train[column_list]), 
+                                columns = column_list_scaled, 
+                                index = train.index)
+    train = train.join(train_scaled)
 
-	scaler, train['age'] = split_scale.my_minmax_scaler(train[['age']])
-	scaler, train['fare'] = split_scale.my_minmax_scaler(train[['fare']])
+    test_scaled = pd.DataFrame(scaler.transform(test[column_list]), 
+                                columns = column_list_scaled, 
+                                index = test.index)
+    test = test.join(test_scaled)
+    
+    return train, test
 
-	return df, LE, train, test
+
