4aea00347affffa6d59c78b3a2ed4910a683f77c
Jacqueline Shay <Jacqueline.A.Shay@gmail.com>
2019-12-08 11:19:04 -0600

    .

--- NLP/acquire_news_articles.py
+++ NLP/acquire_news_articles.py
@@ -1,47 +1,41 @@
- # imports
+# imports
 from requests import get
 from bs4 import BeautifulSoup
 import os
 import pandas as pd
 
-
 def get_news_articles():
-	# store into local csv file or make a new request if file not exist
     filename = 'inshorts_news_articles.csv'
 
-    # check for presence of the file, or make a new request
+    # check for presence of the file or make a new request
     if os.path.exists(filename):
         return pd.read_csv(filename)
     else:
         return make_new_request()
 
 def get_articles_from_topic(url):
-	# provide identifier when requesting webpage
-    headers = {'user-agent': 'JQ'}
-    # request
+    headers = {'user-agent': 'Codeup Bayes Instructor Example'}
     response = get(url, headers=headers)
-    # parse requested content w/ beautifulsoup
     soup = BeautifulSoup(response.content, 'html.parser')
 
     output = []
-    # select the class news-card as a whole
+
     articles = soup.select(".news-card")
-    # filter specific content & store into the variable
+
     for article in articles: 
         title = article.select("[itemprop='headline']")[0].get_text()
-        content = article.select("[itemprop='articleBody']")[0].get_text()
+        body = article.select("[itemprop='articleBody']")[0].get_text()
         author = article.select(".author")[0].get_text()
         published_date = article.select(".time")[0]["content"]
         category = response.url.split("/")[-1]
-        # store each part into a dictionary
+
         article_data = {
             'title': title,
-            'content': content,
+            'body': body,
             'category': category,
             'author': author,
             'published_date': published_date,
         }
-        # store each dictionary into 1 list
         output.append(article_data)
 
 
@@ -49,7 +43,6 @@ def get_articles_from_topic(url):
 
 
 def make_new_request():
-	# retrieve data from every target webpage
     urls = [
         "https://inshorts.com/en/read/business",
         "https://inshorts.com/en/read/sports",
@@ -63,11 +56,7 @@ def make_new_request():
         # We use .extend in order to make a flat output list.
         output.extend(get_articles_from_topic(url))
 
-    print("content")
-    print(output)
-    # store data into df
     df = pd.DataFrame(output)
-    # store df into csv file
     df.to_csv('inshorts_news_articles.csv') 
 
     return df
